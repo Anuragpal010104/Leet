937684bd37194319bf87c99749fc3ae360e65998
Anurag Pal <anuragpal2004@gmail.com>
2023-12-06 11:57:06 +0530

    Added Youtube modal

--- src/components/ProblemsTable/ProblemTable.tsx
+++ src/components/ProblemsTable/ProblemTable.tsx
@@ -1,13 +1,33 @@
 import { problems } from '@/mockProblem/problem';
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsCheckCircle } from 'react-icons/bs';
 import { AiFillYoutube } from 'react-icons/ai';
+import { IoClose } from 'react-icons/io5';
+import YouTube from 'react-youtube';
 
 type Props = {}
 
 function ProblemTable({}: Props) {
+    const [youtubePlayer,setYoutubePlayer] = useState({
+        isOpen: false,
+        videoId: ""
+    })
+    const closeModal = ()=>{
+        setYoutubePlayer({isOpen: false, videoId: ""});
+    }
+    useEffect(()=>{
+        const handleEsc = (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                closeModal();
+            }
+        }
+        window.addEventListener('keydown', handleEsc)
+
+        return () => window.removeEventListener('keydown', handleEsc)
+    })
   return (
+    <>
     <tbody className='text-white'>
         {problems.map((doc,idx)=>{
             const difficultyColor = doc.difficulty === "Easy" ? "text-green-500" : doc.difficulty === "Medium" ? "text-yellow-500" : "text-red-500";
@@ -32,6 +52,7 @@ function ProblemTable({}: Props) {
                             <AiFillYoutube
                             fontSize={"28"}
                             className='cursor-pointer hover:text-red-600'
+                            onClick={()=>setYoutubePlayer({isOpen: true, videoId: doc.videoId as string})}
                             />
                         ) : (
                             <p className='text-gray-400'>Coming soon</p>
@@ -41,6 +62,22 @@ function ProblemTable({}: Props) {
             )
         })}
     </tbody>
+    {youtubePlayer.isOpen && (
+        <div className='fixed top-0 left-0 h-screen w-screen flex items-center justify-center ' >
+            <div className='bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute' onSubmit={closeModal}></div>
+            <div className='w-full z-50 h-full px-6 relative max-w-4xl'>
+                <div className='w-full h-full flex items-center justify-center relative'>
+                    <div className='w-full relative'>
+                        <IoClose fontSize={"35"} className='cursor-pointer absolute -top-16 right-0'
+                        onSubmit={closeModal}  />
+                        <YouTube videoId={youtubePlayer.videoId} loading='lazy' iframeClassName='w-full min-h-[500px]' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )}
+
+    </>
   )
 }
 
